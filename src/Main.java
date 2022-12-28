f216c781282be8a39b4879079ac31226942dc7d2
Fatima Ez-zahra Guebli <bitifati1603@gmail.com>
2022-12-28 07:36:52 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+       // int []tableau = new int[50];
+        // System.out.println();
+    }
+}
---
+++ src/defi_01/Table_odd_even.java
@@ -0,0 +1,9 @@
+package defi_01;
+
+public class Table_odd_even {
+
+    private int n;
+    private int []tab = new int[n];
+
+    // declaration d'un constructeur :
+}
